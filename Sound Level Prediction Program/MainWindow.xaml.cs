cc71f049b088f0e5bd47b7a303c5899a4b1bec20
Lukasz A <lukasz.amroziewicz@gmail.com>
2019-04-14 14:37:01 +0200

    Aktualizacja_14_04_2019

--- Sound Level Prediction Program/MainWindow.xaml.cs
+++ Sound Level Prediction Program/MainWindow.xaml.cs
@@ -31,63 +31,64 @@ namespace Sound_Level_Prediction_Program
 
         private void ElementsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (((ListView)sender).SelectedIndex < 5)
+            if (((ListView)sender).SelectedIndex == 0)
+            { elementslistviewCounter = 4; }
+            else if (((ListView)sender).SelectedIndex == ((ListView)sender).Items.Count - 1)
+            { elementslistviewCounter = ((ListView)sender).Items.Count - 5; }
+            else if (((ListView)sender).SelectedIndex <= ((ListView)sender).Items.Count - 6 && ((ListView)sender).SelectedIndex >= 5)
+            { elementslistviewCounter = ((ListView)sender).SelectedIndex; }
+
+            if (((ListView)sender).SelectedIndex < 5 && elementslistviewCounter < 5)
             {
                 DownButton.IsEnabled = true;
                 UpButton.IsEnabled = false;
             }
-            else if (((ListView)sender).SelectedIndex >= 5 && ElementsListView.SelectedIndex < ElementsListView.Items.Count - 5)
+            else if (((ListView)sender).SelectedIndex >= ((ListView)sender).Items.Count - 5 && elementslistviewCounter >= ((ListView)sender).Items.Count - 5)
             {
                 UpButton.IsEnabled = true;
-                DownButton.IsEnabled = true;
+                DownButton.IsEnabled = false;
             }
             else
             {
                 UpButton.IsEnabled = true;
-                DownButton.IsEnabled = false;
+                DownButton.IsEnabled = true;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (ElementsListView.SelectedIndex > 4 && ElementsListView.SelectedIndex <= 9 
-                && elementslistviewCounter > 4 && elementslistviewCounter <= 9) { elementslistviewCounter = 4; }
-            else if (ElementsListView.SelectedIndex > 9 && elementslistviewCounter > 9) { elementslistviewCounter = ElementsListView.Items.Count - 1; }
-
             if (sender == DownButton)
             {
-                if (elementslistviewCounter + 5 < ElementsListView.Items.Count - 1)
+                if (elementslistviewCounter < ElementsListView.Items.Count - 6)
                 {
                     UpButton.IsEnabled = true;
                     DownButton.IsEnabled = true;
                     ElementsListView.ScrollIntoView(ElementsListView.Items[elementslistviewCounter + 5]);
-
-                    if (elementslistviewCounter + 5 > ElementsListView.Items.Count - 1) { elementslistviewCounter += 5; }
-                    else { elementslistviewCounter = ElementsListView.Items.Count - 1; }
+                    elementslistviewCounter += 5;
                 }
                 else
                 {
                     UpButton.IsEnabled = true;
                     DownButton.IsEnabled = false;
-                    ElementsListView.ScrollIntoView(ElementsListView.Items[elementslistviewCounter]);
+                    ElementsListView.ScrollIntoView(ElementsListView.Items[ElementsListView.Items.Count - 1]);
+                    elementslistviewCounter = ElementsListView.Items.Count - 5;
                 }
             }
             else if (sender == UpButton)
             {
-                if (elementslistviewCounter - 5 > 4)
+                if (elementslistviewCounter - 5 > 0)
                 {
                     UpButton.IsEnabled = true;
                     DownButton.IsEnabled = true;
                     ElementsListView.ScrollIntoView(ElementsListView.Items[elementslistviewCounter - 5]);
-
-                    if (elementslistviewCounter - 5 < 4) { elementslistviewCounter -= 5; }
-                    else { elementslistviewCounter = 4; }
+                    elementslistviewCounter -= 5;
                 }
                 else
                 {
                     UpButton.IsEnabled = false;
                     DownButton.IsEnabled = true;
                     ElementsListView.ScrollIntoView(ElementsListView.Items[0]);
+                    elementslistviewCounter = 4;
                 }
             }
         }
@@ -112,8 +113,12 @@ namespace Sound_Level_Prediction_Program
 
         private void ElementsPopup_Closed(object sender, EventArgs e)
         {
-            elementslistviewCounter = ElementsListView.SelectedIndex;
-            ElementsListView.ScrollIntoView(ElementsListView.SelectedItem);
+            if (ElementsListView.SelectedIndex <= 4)
+            { elementslistviewCounter = 4; ElementsListView.ScrollIntoView(ElementsListView.Items[0]); }
+            else if (ElementsListView.SelectedIndex <= ElementsListView.Items.Count - 6)
+            { elementslistviewCounter = ElementsListView.SelectedIndex; ElementsListView.ScrollIntoView(ElementsListView.SelectedItem); }
+            else { elementslistviewCounter = ElementsListView.Items.Count - 5; ElementsListView.ScrollIntoView(ElementsListView.Items[ElementsListView.Items.Count - 1]); }
+            ElementsListView_SelectionChanged(ElementsListView, null);
         }
     }
 }
